c7ea906ac8102fd566f93ae96b18d06555757e75
Arklok <arklok@Arkloks-MacBook-Air.local>
2022-08-11 16:43:23 -0300

    valor no 9 de novo

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -79,26 +79,7 @@ class Questions extends React.Component {
       const result = (this.difficultyArray())[questionIndex];
       return dificultQuestion(result);
     }
-    // this.addScore();
   }
-
-  // addScore = () => {
-  //   const { restTime, dificult, scoreget, score } = this.props;
-  //   const ten = 10;
-  //   const hardN = 3;
-  //   const testDificult = () => {
-  //     if (dificult === 'hard') { return hardN; }
-  //     if (dificult === 'medium') { return 2; }
-  //     if (dificult === 'easy') { return 1; }
-  //   };
-  //   const soma = () => {
-  //     if (dificult) {
-  //       return (score + (ten + (restTime * testDificult())));
-  //     }
-  //     return 0;
-  //   };
-  //   return scoreget(soma());
-  // }
 
   render() {
     const { questions,
@@ -166,10 +147,6 @@ Questions.propTypes = {
   stop: PropTypes.func.isRequired,
   disableRest: PropTypes.func.isRequired,
   dificultQuestion: PropTypes.func.isRequired,
-  // restTime: PropTypes.string.isRequired,
-  // dificult: PropTypes.string.isRequired,
-  // scoreget: PropTypes.func.isRequired,
-  // score: PropTypes.number.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -180,7 +157,6 @@ const mapDispatchToProps = (dispatch) => ({
   stop: (payload) => (dispatch(stopTime(payload))),
   disableRest: (payload) => (dispatch(buttonDisable(payload))),
   dificultQuestion: (payload) => (dispatch(dificultQuest(payload))),
-  // scoreget: (payload) => (dispatch(getScore(payload))),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Questions);
